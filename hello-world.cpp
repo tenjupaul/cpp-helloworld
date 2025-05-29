ef31648b7b752f3bfea18c3990d939bb0080aed5
Tenju Paul <tenjupaul@gmail.com>
2025-05-28 17:11:21 -0700

    Update hello-world.cpp

--- hello-world.cpp
+++ hello-world.cpp
@@ -10,14 +10,14 @@ int main() {
 
 }
 
-int leak_example(int c) {
-  void *p = malloc(10);
-  if(c)
-    return -1;   // "p" is leaked
-  /* ... */
-  free(p);
-  return 0;
-}
+// int leak_example(int c) {
+//   void *p = malloc(10);
+//   if(c)
+//     return -1;   // "p" is leaked
+//   /* ... */
+//   free(p);
+//   return 0;
+// }
 
 
 // Dead Code example
@@ -29,15 +29,15 @@ int use_p(int p){
     return 0;
 }
 
-// int deadcode_example1(int *p) {
-//     if( p == NULL ) {
-//         return -1;
-//     }
+int deadcode_example1(int *p) {
+    if( p == NULL ) {
+        return -1;
+    }
 
-//     use_p( *p );
-//     if ( p == NULL ) {      // p cannot be null.
-//         handle_error();     // Defect: dead code
-//         return -1;         
-//     }
-//     return 0;
-// }
+    use_p( *p );
+    if ( p == NULL ) {      // p cannot be null.
+        handle_error();     // Defect: dead code
+        return -1;         
+    }
+    return 0;
+}
